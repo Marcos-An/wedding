09beaa12ab755f621bb4877f5cfbc821e3a5876e
Marcos-An <loseonjr@gmail.com>
2022-01-28 08:42:30 -0400

    Mudança de links

--- src/components/molecules/storeButton/index.jsx
+++ src/components/molecules/storeButton/index.jsx
@@ -1,34 +1,34 @@
 import styles from './storeButton.module.scss'
 import Image from 'next/image'
-import Link  from 'next/link'
+import Link from 'next/link'
 
 const MAGALU = `magalu`
 const MAGALU_LOGO = `/magalu-logo.svg`
 const HAVAN_LOGO = `/havan-logo.svg`
 
 
-const MAGALU_URL = `https://lista.havan.com.br/Convidado/ItensListaPresente/592454`
+const MAGALU_URL = `https://www.querodecasamento.com.br/lista-de-casamento/leticia-dos-santos-rodrigues-marcos-antonio-da-silva-junior`
 const HAVAN_URL = `https://lista.havan.com.br/Convidado/ItensListaPresente/592454`
 
-export default function StoreButton({text, store}) {
+export default function StoreButton({ text, store }) {
 
   const redirectURL = () => {
     return store === MAGALU ? MAGALU_URL : HAVAN_URL
   }
-  
+
   return (
-    <Link href={redirectURL()} passHref > 
-     <a target="_blank" className={styles.storeButton}>
-      <div  className={styles.logo}>
-        <Image 
-          src={store === MAGALU ? MAGALU_LOGO : HAVAN_LOGO}
-          layout='fill'
-          objectFit='contain'
-        />
-      </div>
+    <Link href={redirectURL()} passHref >
+      <a target="_blank" className={styles.storeButton}>
+        <div className={styles.logo}>
+          <Image
+            src={store === MAGALU ? MAGALU_LOGO : HAVAN_LOGO}
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
         {text}
-        
-     </a>
+
+      </a>
     </Link>
   )
 }
--- src/components/molecules/whatsappButton/index.jsx
+++ src/components/molecules/whatsappButton/index.jsx
@@ -1,24 +1,24 @@
 import styles from './whatsappButton.module.scss'
 import Image from 'next/image'
-import Link  from 'next/link'
- 
- 
+import Link from 'next/link'
+
+
 export default function WhatsappButton() {
- 
-  const WHATSAPP_URL = 'https://api.whatsapp.com/send?phone=5567999290375&text=' 
-  
+
+  const WHATSAPP_URL = 'https://api.whatsapp.com/send?phone=556792040761&text='
+
   return (
-    <Link href={WHATSAPP_URL} passHref > 
-     <a target="_blank" className={styles.whatsappButton}>
-      <div  className={styles.logo}>
-        <Image 
-          src={'/whatsapp-logo.svg'}
-          layout='fill'
-          objectFit='contain'
-        />
-      </div>
-        Confirme sua preseça 
-     </a>
+    <Link href={WHATSAPP_URL} passHref >
+      <a target="_blank" className={styles.whatsappButton}>
+        <div className={styles.logo}>
+          <Image
+            src={'/whatsapp-logo.svg'}
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
+        Confirme sua preseça
+      </a>
     </Link>
   )
 }
